e854061d2952bda25baa8e1dc92b7a8448018107
Adam Risberg <myniztan@hotmail.com>
2017-12-01 00:51:27 -0600

    Add GET /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,5 +1,5 @@
 const {ObjectID} = require("mongodb");
-const {mongoos} = require("./../server/db/mongoose");
+const {mongoose} = require("./../server/db/mongoose");
 const {Todo} = require("./../server/models/todo");
 const {User} = require("./../server/models/user");
 
